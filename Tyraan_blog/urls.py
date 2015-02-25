ff1040d3cd26eaccd05c65dd829a8276a5cd9a07
Tyraan T <tomkly000@gmail.com>
2015-02-25 17:11:58 +0800

    sdas
    dasda

--- Tyraan_blog/urls.py
+++ Tyraan_blog/urls.py
@@ -9,10 +9,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Tyraan_blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
-    url(r'^admin/', include(admin.site.urls)),   
-    
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),    
+    url(r'^admin/', include(admin.site.urls)),     
     url(r'^$', 'tyraan.views.blog_list',name='blog_list'), # Ê×Ò³
     url(r'^bloglist/$','tyraan.views.blog_list',name='blog_list'),
     url(r'^article/(?P<slug>[-\w]+)/$','tyraan.views.blog_show',name='blog_article'), # ²©ÎÄ
